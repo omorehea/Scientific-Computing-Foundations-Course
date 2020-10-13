3eccc6a2d7306211346ba452824498bca77a551e
Owen Morehead <omorehea@ucsc.edu>
2020-10-13 13:22:25 -0700

    Final touches to check_dirSize.sh

--- Homework/hw1/code/check_dirSize.sh
+++ Homework/hw1/code/check_dirSize.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+#First, go to directory that stores my local am129 directory
+#Then use du on this directory, sort the contents by size in decreasing order
+#store the 3 largest entries to dirSizes.txt, and output the entire list to screen
+
+
 cd ../../../../
 du moreheadowen-am129-fall2020 | sort -g -r | head -n 3 > dirSizes.txt
-cat dirSizes.txt
+du moreheadowen-am129-fall2020 | sort -g -r
 
