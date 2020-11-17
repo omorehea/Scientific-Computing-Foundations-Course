e819edc869912ae5e56ad3b22cc4e14208f95abe
Owen Morehead <omorehea@ucsc.edu>
2020-11-17 11:52:21 -0800

    Finished first version of mathieuRun.py

---
+++ Homework/hw5/code/mathieu/mathieu.f90
@@ -0,0 +1,56 @@
+! File: mathieu.f90
+! Author: Ian May
+! Purpose: Compute Mathieu functions of the first kind numerically
+
+program mathieu
+  use utility, only : dp, pi
+  use problemsetup, only : Npts, qIdx, outFile, problemsetup_Init
+  use diffop, only : diffop_Mathieu
+  implicit none
+  ! Size of transform
+  ! Problem data
+  integer :: info=0
+  integer :: lwork
+  real (dp) :: dx
+  real (dp), allocatable :: x(:)
+  real (dp), allocatable :: wr(:), wi(:)
+  real (dp), allocatable :: work(:)
+  real (dp), allocatable :: H(:,:), vr(:,:), vl(:,:)
+  ! Loop variables
+  integer :: i,j
+  ! Read in the input file
+  call problemsetup_Init('mathieu.init.bak')
+  ! Set local variables
+  lwork = 8*Npts
+  dx = 2*pi/Npts
+  ! Allocate arrays to match problem size
+  allocate(x(Npts))
+  allocate(wr(Npts))
+  allocate(wi(Npts))
+  allocate(work(lwork))
+  allocate(H(Npts,Npts))
+  allocate(vl(Npts,Npts))
+  allocate(vr(Npts,Npts))
+  ! Set up physical domain and signal
+  do i=1,Npts
+    x(i) = (i-1)*dx
+  end do
+  ! Get Mathieu operator
+  call diffop_Mathieu(qIdx,x,H)
+  ! Use Lapack to find the eigenvalues/eigenvectors of H
+  call dgeev('N','V',Npts,H,Npts,wr,wi,vl,1,vr,Npts,work,lwork,info)
+  ! Open the output file and write the columns
+  open(20,file=outFile,status="replace")
+  do i=1,Npts
+    write(20,*) x(i), wr(i), wi(i), (vr(i,j), j=1,Npts)
+  end do
+  close(20)
+  ! Deallocate all used space
+  deallocate(x)
+  deallocate(wr)
+  deallocate(wi)
+  deallocate(work)
+  deallocate(H)
+  deallocate(vl)
+  deallocate(vr)
+end program mathieu
---
+++ Homework/hw5/code/mathieuRun.py
@@ -0,0 +1,75 @@
+#am129/hw5/code/mathieuRun.py
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def rebuild():
+
+    #change into mathieu
+    os.chdir('mathieu')
+    #call make clean mathieu
+    cmd1 = 'make clean mathieu'
+    os.system(cmd1)
+    #change back to code directory
+    os.chdir('../')
+
+def build():
+    os.chdir('mathieu')
+    os.system('make mathieu')
+    os.chdir('../')
+
+def generate_input(N,q):
+    if 'mathieu.init' in os.listdir('mathieu'):
+        os.rename('mathieu/mathieu.init','mathieu/mathieu.init.bak')
+    fp = open('mathieu/mathieu.init.bak','w')
+    fp.write('num_points ' + str(N) + '\n')
+    fp.write('q_index ' + str(q) + '\n')
+    fp.write('run_name Mathieu_' + str(N) +'_' + str(q))
+    fp.close()
+
+def run_mathieu(N,q):
+    build()
+    generate_input(N,q)
+    os.chdir('mathieu')
+    os.system('./mathieu')
+    os.chdir('../')
+
+def parsweep_mathieu(N):
+    #Range of q values to test
+    qRange = np.arange(0,42,2)
+    for q in qRange:
+        run_mathieu(N,q)
+
+def plot_parsweep(N,nPlot):
+    #Set range and space to store data
+    qRange = np.arange(0,42,2)
+    evals = np.zeros([len(qRange),nPlot])
+    #Open files and extract relevant data
+    for idx,q in enumerate(qRange):
+        fname = 'mathieu/data/Mathieu_' + str(N) + '_' + str(q) + '.dat'
+        data = np.loadtxt(fname)
+        evals[idx,:] = np.sort(data[:,1])[0:nPlot]
+    #Generate a plot of eigenvalues as a function of parameter q
+    plt.figure(figsize=(12,8))
+#    plt.plot(qRange,evals,'-k')
+    a = lambda: [1 + q - (1/8)*q**2 - (1/64)*q**3 for q in range(0,42,2)]
+    b = lambda: [4 - (1/12)*q**2 + (5/13824)*q**4 for q in range(0,42,2)]
+    plt.plot(qRange,a(),'--b',label='a(q)')
+    plt.plot(qRange,b(),'.-b',label='b(q)')
+    plt.grid(True)
+    plt.title('a(q), b(q) vs q')
+    plt.xlabel('q')
+    plt.ylabel('Eigenvalues')
+    plt.legend()
+    plt.show()
+
+if __name__=="__main__":
+   
+    N = 101
+    rebuild()
+    parsweep_mathieu(N)
+    plot_parsweep(N,7)
+
+
